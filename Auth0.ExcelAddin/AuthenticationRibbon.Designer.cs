7001cf7110011deb808a7c74ac9ad0d5c3156a67
Shawn G. Meyer <sgmeyer@gmail.com>
2018-02-28 13:52:53 -0800

    Initial commit

---
+++ Auth0.ExcelAddin/AuthenticationRibbon.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Auth0.ExcelAddin
+{
+    partial class AuthenticationRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public AuthenticationRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AuthenticationRibbon));
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.btnLogin = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.btnLogin);
+            this.group1.Label = "Auth0";
+            this.group1.Name = "group1";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnLogin.Image = ((System.Drawing.Image)(resources.GetObject("btnLogin.Image")));
+            this.btnLogin.Label = "Login";
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.ShowImage = true;
+            this.btnLogin.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnLogin_Click);
+            // 
+            // AuthenticationRibbon
+            // 
+            this.Name = "AuthenticationRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.AuthenticationRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnLogin;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal AuthenticationRibbon AuthenticationRibbon
+        {
+            get { return this.GetRibbon<AuthenticationRibbon>(); }
+        }
+    }
+}
---
+++ Auth0.ExcelAddin/AuthenticationRibbon.cs
@@ -0,0 +1,34 @@
+﻿using System.Configuration;
+using System.Threading;
+using System.Windows.Forms;
+using Auth0.OidcClient;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace Auth0.ExcelAddin
+{
+    public partial class AuthenticationRibbon
+    {
+        private void AuthenticationRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private async void btnLogin_Click(object sender, RibbonControlEventArgs e)
+        {
+            if (SynchronizationContext.Current == null)
+            {
+                SynchronizationContext.SetSynchronizationContext(new WindowsFormsSynchronizationContext());
+            }
+
+            var options = new Auth0ClientOptions
+            {
+                ClientId = ConfigurationManager.AppSettings["Auth0ClientId"],
+                Domain = ConfigurationManager.AppSettings["Auth0Domain"]
+            };
+
+            var oidClient = new Auth0Client(options);
+
+            var result = await oidClient.LoginAsync();
+        }
+    }
+}
